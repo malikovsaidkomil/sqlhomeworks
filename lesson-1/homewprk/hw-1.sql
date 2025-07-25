275b2e5901e254b45ebee2b9a90cd53e0f946b5b
malikovsaidkomil <malikovsaidkomil035@gmail.com>
2025-07-25 17:21:17 +0500

    Create hw-1.sql

---
+++ lesson-1/homewprk/hw-1.sql
@@ -0,0 +1,13 @@
+data- "komil",28,true.
+database - students,teachers,studentsid.
+  relational database- studentsid
+table-students-studentsid,name,age.
+   different authentication modes in sql server
+windows authentication – uses windows credentials (ative directory).
+sql server authentication – uses sqllogin/password (stored in sql server).
+(optional) mixed mode authentication – supports both.
+ create database schooldb;
+create table students(studentid int, primary key,name varchar(50),age int)
+ sql server,ssms sql server management studio, sql structured query language.
+dql (data query language) select, dml (data mnipulation language)insert update delete,ddl (data definition language)create alter drop truncate, 
+  dcl (data control language)grant revoke,tcl(transaction control language)begin transaction commit rollback
